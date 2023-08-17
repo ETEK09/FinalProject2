e9916c321ae9dac5fea312fb1b7a07afc828e8f6
Eric Caro <c.aroericm@gmail.com>
2023-08-16 19:52:33 -0500

    init commit

---
+++ CSharp76/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSharp38.Model
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
